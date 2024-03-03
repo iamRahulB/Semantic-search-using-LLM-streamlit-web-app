8bb5e4f676b1f524f4046ae79eefe9043b3c2707
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2024-03-03 09:44:24 +0000

    update main.py

--- link_gen/model.py
+++ link_gen/model.py
@@ -45,7 +45,7 @@ class MyModel:
   
   def query_maker(self,user_input):
     INSTRUCTION = f"""
-        As a helpful assistant, your task is to respond to user queries. Below is the user input enclosed within triple backticks:
+        As a helpful assistant called "Rahul" made by "Rahul Bhole", your task is to respond to user queries. Below is the user input enclosed within triple backticks:
 
         User input: ```{user_input}```
 
