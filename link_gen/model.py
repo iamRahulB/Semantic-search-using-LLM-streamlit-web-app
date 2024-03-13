1541a9415273b9e5ef7b423d6eabecfb18eab561
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2024-03-13 18:19:34 +0000

    Revert "updated instructions"
    This reverts commit d40e141f30abcb8dcf4159b17b0b2eddc3edbc1f.

--- link_gen/model.py
+++ link_gen/model.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 import pandas as pd
 
+<<<<<<< HEAD
 from langchain_community.vectorstores import FAISS
 
 import streamlit as st
@@ -49,6 +50,14 @@ template = """
 #     please take "last mesage history of user and AI" this in mind for the chat history related questions...
 
 #     please take "semantic search from history" in mind for the contextual understanding 
+=======
+generation_config = genai.types.GenerationConfig(
+    temperature=1,
+    top_p=1,
+    top_k=1,
+    max_output_tokens=4000,
+)
+>>>>>>> parent of d40e141 (updated instructions)
 
 
 # """
@@ -62,6 +71,7 @@ template = """
 #  semantic searched result for user query : {user_input_semantic_search}
 
 
+<<<<<<< HEAD
   
    
 # """
@@ -92,6 +102,28 @@ template2 = """
     Your objective is to generate a concise and grammatically correct query based on the user's question. This query will be used to search for the latest details on Google. When generating the query, ensure it reads naturally and includes terms like "latest," "recent," or "2024" to indicate the search for up-to-date information. Imagine yourself as the user and phrase the query in a way that you would search for the given user input on Google to find the latest updates.
 
     Also just give me generated query in response Don't give anything else other than query. 
+=======
+class MyModel:
+
+  def __init__(self):
+    pass
+
+  def run_gemini(self, user_input):
+
+    INSTRUCTION = f"""
+    As a helpful chatbot that chats with user called "Rahul" made by "Rahul Bhole", your task is to respond to user queries. Below is the user input enclosed within triple backticks:
+
+    User input: ```{user_input}```
+
+    by understanding the intent of the user's query, decide if you can answer this or not. If the requested information by user is available in your knowledge base, respond with your response. 
+    2. user might as about chat history, say i dont have memory./
+
+    
+    for the general questions, provide a relevant response instead of "not found".\
+    If the requested information is not available in your knowledge base and if the users question is valid for google search then simply say "not found" in small letters.\
+    when responding with "not found" ensure it is appropriate google query. for the queries that can be searched on google, repond with the "not found".\
+    for the general questions, provide a relevant response instead of "not found".
+>>>>>>> parent of d40e141 (updated instructions)
 """
 
 
