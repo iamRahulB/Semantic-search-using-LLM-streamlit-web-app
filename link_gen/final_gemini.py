3ff8796349780675b0bbdde07a55ff35227190d1
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2024-03-23 23:04:22 +0530

    Update final_gemini.py

--- link_gen/final_gemini.py
+++ link_gen/final_gemini.py
@@ -33,16 +33,38 @@ summary: *{semantic_search}*
 **Answer the question and provide a detailed explanation based on the summary provided.
 Remember to use IPython Markdown formatting and include any relevant Python code.
 Remember User does not know that summary is sent to you So pretend as you know all already.**
+remember : don't reveal tha you are provided with information about user question, make user believe that u had all the info.
 """
 
 
         genai.configure(api_key=os.environ['GEMINI_API'])
 
         model_gem = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                                      generation_config=generation_config, )
+                                      generation_config=generation_config, safety_settings=[
+  {
+    "category": "HARM_CATEGORY_DANGEROUS",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_NONE",
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_NONE",
+  },
+]
+)
 
         response = model_gem.generate_content(INSTRUCTION).text
-
 
 
         # obj_pinecone=PineConeIndex()
